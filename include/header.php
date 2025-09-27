a609d678f9293ca189c87d5c584c97bfe1420cb1
Aditya Singh <singhas1418@gmail.com>
2025-09-27 13:23:42 +0530

    adding headers

--- include/header.php
+++ include/header.php
@@ -43,7 +43,7 @@
                                         <a href="tel:+919211018618">
                                             <span class="color-secondary">Email: </span> <span><span
                                                     class="__cf_email__"
-                                                    data-cfemail="67300614130e06275008150808014904080a">[&#160; info@shivglobeacademy.com]</span></span>
+                                                    data-cfemail="67300614130e06275008150808014904080a">[&#160; ]</span></span>
                                         </a>
                                     </li>
                                     <li>
